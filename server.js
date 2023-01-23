ebdfc6163ad523c54ba65f9eed4371aa743b2406
Kevin Tran <kevtran@hotmail.com>
2023-01-23 00:05:44 -0800

    Add working email server

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 // BackEnd
 
+require("dotenv").config();
 const express = require("express");
 const router = express.Router();
 const cors = require("cors");
 const nodemailer = require("nodemailer");
+
+const { USER, PASSWORD, SERVICE } = process.env;
 
 const app = express();
 app.use(cors());
@@ -17,8 +20,32 @@ app.listen(6969, () => {
 });
 
 const myEmail = nodemailer.createTransport({
-    service: hotmail,
+    service: SERVICE,
     auth: {
-        user: 
-    }
+        user: USER,
+        pass: PASSWORD,
+    },
+});
+
+router.post("/contact", (req, res) => {
+    const { firstName, lastName, email, message } = req.body;
+    const fullName = firstName + " " + lastName;
+
+    const mail = {
+        from: USER,
+        to: USER,
+        subject: `Portfolio Site - Contact Request - ${fullName}`,
+        html: `<h3>Email sent from portfolio site </h3>
+            <p>Name: ${fullName}</p>
+           <p>Email: ${email}</p>
+           <p>Message: ${message}</p>`,
+    };
+
+    myEmail.sendMail(mail, (error) => {
+        if (error) {
+            res.json(error);
+        } else {
+            res.json({ code: 200, status: "Moo Moo" });
+        }
+    });
 });
